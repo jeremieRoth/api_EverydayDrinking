696f60637bd17752103ab3e69b491a1da3eaf904
gilian dupuis <gilian.dupuis@orange.fr>
2017-08-01 21:37:58 +0200

    add findById in routes

--- app/routes.php
+++ app/routes.php
@@ -25,8 +25,43 @@ $app->get('/establishments', function() use ($app){
     return $app->json($responseData);
 });
 
+$app->get('/establishment/{id}', function() use ($app){
+    $establishments = $app['dao.establishment']->find($id);
+	if(!isset($establishments)){
+		$app->abort(404, 'establishments not exist');
+	}
+    $responseData = array();
+	foreach ($establishments as $establishment) {
+		$responseData[] = array(
+			'id' => $establishment->getId(),
+			'name' => $establishment->getName(),
+			'location' => $establishment->getLocation()
+		);
+	}
+    return $app->json($responseData);
+});
+
+
 $app->get('/comments', function() use ($app){
     $comments = $app['dao.comment']->findAll();
+    $responseData = array();
+	foreach ($comments as $comment) {
+		$responseData[] = array(
+			'id' => $comment->getId(),
+			'user' => $comment->getUser(),
+			'comment' => $comment->getComment(),
+            'score' => $comment->getScore(),
+            'establishment' => $comment->getEstablishment()
+		);
+	}
+    return $app->json($responseData);
+});
+
+$app->get('/comment/{id}', function() use ($app){
+    $comments = $app['dao.comment']->findAll();
+	if(!isset($comments)){
+		$app->abort(404, 'comment not exist');
+	}
     $responseData = array();
 	foreach ($comments as $comment) {
 		$responseData[] = array(
@@ -54,6 +89,23 @@ $app->get('/drinks', function() use ($app){
     return $app->json($responseData);
 });
 
+$app->get('/drink/{id}', function() use ($app){
+    $drinks = $app['dao.drink']->findAll();
+	if(!isset($drinks)){
+		$app->abort(404, 'drink not exist');
+	}
+    $responseData = array();
+	foreach ($drinks as $drink) {
+		$responseData[] = array(
+			'id' => $drink->getId(),
+			'name' => $drink->getName(),
+			'price' => $drink->getPrice(),
+            'establishment' => $drink->getEstablishment()
+		);
+	}
+    return $app->json($responseData);
+});
+
 $app->get('/locations', function() use ($app){
     $locations = $app['dao.location']->findAll();
     $responseData = array();
@@ -67,8 +119,41 @@ $app->get('/locations', function() use ($app){
     return $app->json($responseData);
 });
 
+$app->get('/location/{id}', function() use ($app){
+    $locations = $app['dao.location']->findAll();
+	if(!isset($locations)){
+		$app->abort(404, 'location not exist');
+	}
+    $responseData = array();
+	foreach ($locations as $location) {
+		$responseData[] = array(
+			'id' => $location->getId(),
+			'lontitude' => $location->getLongitude(),
+			'latitude' => $location->getLatitude()
+		);
+	}
+    return $app->json($responseData);
+});
+
 $app->get('/users', function() use ($app){
     $users = $app['dao.user']->findAll();
+    $responseData = array();
+	foreach ($users as $user) {
+		$responseData[] = array(
+			'id' => $user->getId(),
+			'login' => $user->getLogin(),
+			'password' => $user->getPassword(),
+            'username' => $user->getUserName()
+		);
+	}
+    return $app->json($responseData);
+});
+
+$app->get('/user/{id}', function() use ($app){
+    $users = $app['dao.user']->findAll();
+	if(!isset($users)){
+		$app->abort(404, 'user not exist');
+	}
     $responseData = array();
 	foreach ($users as $user) {
 		$responseData[] = array(
